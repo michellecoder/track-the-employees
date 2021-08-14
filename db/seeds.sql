48e11d41d7c67d02a7cba902fa7f7629dd2275bf
Michelle Oakley <michellecoder38@gmail.com>
2021-08-13 19:40:33 -0700

    added comments

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+--Manager id has to use an emplyee id that already exsits "the first seeds for manager id needs to be NULL"
+
+--write DEPARTMENT, ROLE, EMPLOYEE in that order
+
+--values the right data type
