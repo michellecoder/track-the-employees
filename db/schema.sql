f69a67fb44fc49682378f28ccd595f78490e5878
Michelle Oakley <michellecoder38@gmail.com>
2021-08-13 19:40:13 -0700

    fixed spelling

---
+++ db/schema.sql
@@ -0,0 +1,29 @@
+-- Database name
+DROP DATABASE IF EXISTS employee_db;
+CREATE DATABASE employee_db;
+
+USE employee_db;
+
+-- Tables
+CREATE TABLE department(
+  id_department INT PRIMARY KEY NOT AUTO_INCREMENT,
+  department_name VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE role(
+  id_role INT PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary DECIMAL,
+  department_id INT,
+  FOREIGN KEY (department_id) REFERENCES (id_department) ON DELETE CASCADE
+);
+
+CREATE TABLE employee(
+id_employee INT PRIMARY KEY AUTO_INCREMENT,
+first_name VARCHAR(30) NOT NULL,
+last_name VARCHAR(30) NOT NULL,
+role_id INT,
+manager_id INT NULL,
+FOREIGN KEY (role_id) REFERENCES role(id_role) ON DELETE CASCADE,
+FOREIGN KEY (manager_id) REFERENCES employee(id_employee) ON DELETE SET NULL
+);
