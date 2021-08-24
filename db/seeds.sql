85da0b61319d3a93148221d8163d32d5e17f0865
Michelle Oakley <michellecoder38@gmail.com>
2021-08-24 14:06:42 -0700

    adjusted commas and semicolen's

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,18 +1,20 @@
---Manager id has to use an emplyee id that already exsits "the first seeds for manager id needs to be NULL"
 
---write DEPARTMENT, ROLE, EMPLOYEE in that order
 
---values the right data type
+USE employee_db;
 
 INSERT INTO department (department_name)
 VALUES ("accounting"),
 ("HR"),
-("IT");
+("IT"),
+("Billing");
+
+
 
 INSERT INTO role (title,salary,department_id)
 VALUES ("accountant",8000,1),
-("hiring manager",8500,2),
+("hiring manager",85000,2),
 ("frontdesk representitive",90000,3);
 
 INSERT INTO employee (first_name,last_name,role_id,manager_id)
-VALUES ("Sam","Thompson",1,NULL);
+VALUES ("Sam","Thompson",2,NULL),
+("Goku","Dragonball", 3, 1);
